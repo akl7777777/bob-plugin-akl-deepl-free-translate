1835471b6564c52241048922fdb7bc7cb0ab9b8d
akl7777777 <84266551+akl7777777@users.noreply.github.com>
2023-02-17 12:20:19 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,19 @@
+const supportedLanguages = [
+    ['auto', 'auto'],
+    ['zh-Hans', 'zh'],
+    ['en', 'en'],
+    ['de', 'de'],
+    ['fr', 'fr'],
+    ['it', 'it'],
+    ['ja', 'ja'],
+    ['es', 'es'],
+    ['nl', 'nl'],
+    ['pl', 'pl'],
+    ['pt', 'pt'],
+    ['ru', 'ru'],
+];
+
+
+
+exports.supportedLanguages = supportedLanguages;
+
