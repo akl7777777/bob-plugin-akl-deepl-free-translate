0861c22a0afbffbc63c4b49b570a103fc56eb21c
akl7777777 <akl7777777@163.com>
2023-02-26 23:50:37 +0800

    代码优化

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ var utils = require('./utils.js');
 // 出参格式:
 // {"rawData":{},"data":{"jsonrpc":"2.0","id":194187000,"result":{"texts":[{"text":"参数","alternatives":[{"text":"Params"},{"text":"参数表"},{"text":"参量"}]}],"lang":"EN","lang_is_confident":false,"detectedLanguages":{"SK":0.011904,"ZH":0.005038,"unsupported":0.505983,"PT":0.040255,"PL":0.020479,"SL":0.024711999999999994,"DE":0.02167,"RO":0.013807,"ES":0.016012,"TR":0.027060000000000008,"NB":0.022891,"ET":0.016899,"EL":0.001863,"FI":0.016634,"SV":0.030546,"FR":0.017381,"NL":0.022213,"HU":0.017405,"CS":0.014604000000000002,"EN":0.037805,"DA":0.013585999999999997,"IT":0.024718,"JA":0.0064249999999999976,"BG":0.002212,"LT":0.023487,"ID":0.012715,"UK":0.001442,"KO":0.001697,"RU":0.004128,"LV":0.024428}}}
 
-function init_data(source_lang, target_lang) {
+function initData(source_lang, target_lang) {
   return {
     jsonrpc: '2.0',
     method: 'LMT_handle_texts',
@@ -20,7 +20,7 @@ function init_data(source_lang, target_lang) {
   };
 }
 
-function get_i_count(translate_text) {
+function getICount(translate_text) {
   return translate_text.split('i').length - 1;
 }
 
@@ -29,17 +29,15 @@ function getRandomNumber() {
   return rand * 1000;
 }
 
-function getTimeStamp(i_count) {
+function getTimeStamp(iCount) {
   const ts = Date.now();
-  if (i_count !== 0) {
-    i_count = i_count + 1;
-    return ts - (ts % i_count) + i_count;
+  if (iCount !== 0) {
+    iCount = iCount + 1;
+    return ts - (ts % iCount) + iCount;
   } else {
     return ts;
   }
 }
-
-let id = getRandomNumber();
 
 function supportLanguages() {
   return config.supportedLanguages.map(([standardLang]) => standardLang);
@@ -60,31 +58,23 @@ function translate(query, completion) {
     const source_lang = sourceLanguage || 'ZH';
     const target_lang = targetLanguage || 'EN';
     const translate_text = query.text || '';
-    let response;
     if (translate_text !== '') {
       const url = 'https://www2.deepl.com/jsonrpc';
-      //   id = id + 1;
-      id = getRandomNumber()
-      const post_data = init_data(source_lang, target_lang);
+      let id = getRandomNumber()
+      const post_data = initData(source_lang, target_lang);
       const text = {
         text: translate_text,
         requestAlternatives: 3
       };
       post_data.id = id;
       post_data.params.texts = [text];
-      post_data.params.timestamp = getTimeStamp(get_i_count(translate_text));
+      post_data.params.timestamp = getTimeStamp(getICount(translate_text));
       let post_str = JSON.stringify(post_data);
       if ((id + 5) % 29 === 0 || (id + 3) % 13 === 0) {
         post_str = post_str.replace('"method":"', '"method" : "');
       } else {
         post_str = post_str.replace('"method":"', '"method": "');
       }
-      const options = {
-        method: 'POST',
-        url: url,
-        header: { 'Content-Type': 'application/json' },
-        body: $data.fromUTF8(post_str)
-      };
       try {
         $http.request({
           method: "POST",
@@ -114,7 +104,6 @@ function translate(query, completion) {
         });
       }
       catch (e) {
-        $log.error('接口请求错误 ==> ' + JSON.stringify(e))
         Object.assign(e, {
           _type: 'network',
           _message: '接口请求错误 - ' + JSON.stringify(e),
@@ -123,7 +112,6 @@ function translate(query, completion) {
       }
     }
   })().catch((err) => {
-    $log.error('***********解析返回值异常==>' + JSON.stringify(err))
     completion({
       error: {
         type: err._type || 'unknown',
